04500dda808764deed8b3d2b973f337221652ffe
Dmitry <dmitrychues2312@gmail.com>
2023-12-03 17:08:01 +0300

    Commit - 2

--- src/test/Test.tsx
+++ src/test/Test.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Test = () => {
 	return (
 		<div>
-			Test
+			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores quos optio eum nulla consequatur repellat consectetur maxime quidem, aspernatur corrupti ipsa iusto, quaerat esse eius dolor quae delectus consequuntur et!
 		</div>
 	)
 }
