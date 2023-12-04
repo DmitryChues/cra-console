a8e2823b0cee7c13397a19947d066946ea7f5faa
Dmitry <dmitrychues2312@gmail.com>
2023-12-04 19:15:40 +0300

    add p

--- src/test/Test.tsx
+++ src/test/Test.tsx
@@ -3,7 +3,11 @@ import React from "react";
 export const Test = () => {
 	return (
 		<div>
+			<p>1</p>
+			<p>2</p>
+			<p>3</p>
 			Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores quos optio eum nulla consequatur repellat consectetur maxime quidem, aspernatur corrupti ipsa iusto, quaerat esse eius dolor quae delectus consequuntur et!
 		</div>
 	)
 }
+
